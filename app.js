cac09591a8ad7b06409dd6ab30da77aa66e7bcff
kerembakir <kerembakir@gmail.com>
2017-06-06 15:32:31 +0200

    Added sequelize models from class rep.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var pug = require ('pug');
 var sequelize = require('sequelize');
 var session = require('express-session');
-var bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -18,9 +18,75 @@ app.use(session({
 	saveUninitialized: false
 }));
 
-app.set('views', './src/views');
+app.set('views', './views');
 app.set('view engine', 'pug');
 
 
+//Sequelize settings (from class rep)
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('blogapp', process.env.POSTGRES_USER, null, {
+	host: 'localhost',
+	dialect: 'postgres',
+	define: {
+		timestamps: false
+	}
+});
+
+
+
+// Seq. Models
+
+var User = sequelize.define('users', {
+	name: {
+		type: Sequelize.TEXT,
+		allowNull: false,
+		unique: true
+	},
+	email: {
+		type: Sequelize.TEXT,
+		allowNull: false,
+		unique: true
+	},
+	password: Sequelize.TEXT,
+});
+
+var Post = sequelize.define('posts', {
+	title: Sequelize.TEXT,
+	body: Sequelize.TEXT,
+	timeStamp: Sequelize.DATE,
+});
+
+var Comment = sequelize.define('comments', {
+	body: Sequelize.TEXT,
+	timeStamp: Sequelize.DATE,
+});
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000);
-console.log('BB-App running on port 3000');
+console.log('BA-App running on port 3000');
