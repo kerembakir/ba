c39d3aeeb56cc3a32e93f62ed7119943b09d0775
kerembakir <kerembakir@gmail.com>
2017-06-12 17:30:40 +0200

    Changed pug's, adding bcrypt

--- app.js
+++ app.js
@@ -361,6 +361,31 @@ app.get('/myposts', (request,response) =>{
   //   });
   // });
 
+//bcrypt
+
+var password = process.argv[2]; // get their password from the user registration form
+bcrypt.hash(password, 8, function(err, hash) {
+  if (err !== undefined) {
+    console.log(err);
+  } else {
+    sequelize.writeFile("file.txt", hash); // store it in the database
+  }
+});
+
+
+var password = process.argv[2]; // get their password from the user login form
+fs.readFile('file.txt', function(err, data) { // equivalent to getting the user from the database
+  bcrypt.compare(password, data.toString(), function(err, result) {
+    if (err !== undefined) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+});
+
+
+
 
 
 
